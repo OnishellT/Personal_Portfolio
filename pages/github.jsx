e1f7ed790e29af91ffddc67f75a590da26fb3282
OnishellT <onishelltib@gmail.com>
2022-09-02 15:20:09 -0400

    Fixed repos

--- pages/github.jsx
+++ pages/github.jsx
@@ -70,7 +70,6 @@ export async function getStaticProps() {
   );
   let repos = await repoRes.json();
   repos = repos
-    .sort((a, b) => b.stargazers_count - a.stargazers_count)
     .slice(0, 6);
 
   return {
